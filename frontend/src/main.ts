7e7abf8eccc78f75a9c7b87c2f6d5af2e4e5e1ea
Valerii Navalnyi <valeranavalni@gmail.com>
2026-05-09 21:24:42 +0100

    Add Inter font support

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+// Supports weights 100-900
+import '@fontsource-variable/inter/wght.css';
 
 bootstrapApplication(App, appConfig).catch((err) =>
   console.error(err),
