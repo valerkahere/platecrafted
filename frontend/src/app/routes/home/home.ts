99dfafd39dfadd462bd4b4e65ae6827524fad161
Valerii Navalnyi <valeranavalni@gmail.com>
2026-05-12 14:15:56 +0100

    Add bg color. Remove add-item imports

--- frontend/src/app/routes/home/home.ts
+++ frontend/src/app/routes/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { Additem } from '../../components/additem/additem';
 import { Listitems } from '../../components/listitems/listitems';
 import { Search } from '../../components/search/search';
 
 @Component({
   selector: 'app-home',
-  imports: [Additem, Listitems, Search],
+  imports: [ Listitems, Search],
   templateUrl: './home.html',
   styles: [],
 })
